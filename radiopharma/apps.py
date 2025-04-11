47beb4dd8c09f7611d2f89233cc957f162b0da38
oberth-effect <stepan.venclik@gmail.com>
2025-04-11 19:16:33 +0200

    Isotopes => radiopharma

--- radiopharma/apps.py
+++ radiopharma/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IsotopesConfig(AppConfig):
+class RadiopharmaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'isotopes'
+    name = 'radiopharma'

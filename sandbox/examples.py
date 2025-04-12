aa562bcf0ea495b58ff602dc54a279c5e43983f5
Tomáš Červeň <to.cerven@gmail.com>
2025-04-12 21:36:49 +0200

    Examples

---
+++ sandbox/examples.py
@@ -0,0 +1,9 @@
+scenario_1 = {
+    'patients': [
+        Patient(id="a", weight=80, procedure=PROC[Timestamp.FDGB]),
+        Patient(id="b", weight=50, procedure=PROC[Timestamp.Methionin_1]),
+        Patient(id="c", weight=120, procedure=PROC[Timestamp.Methionin_1]),
+    ]
+    'counts': [0, 1, 0, 0, 0, 2]
+    'schedule': []
+}
